c70bae21d248e044b3b0ea9d324e31017cd41703
Raimundo Olate <raimundolatetoro@hotmail.com>
2023-07-18 15:49:43 -0400

    se agregan al admin.py

--- Laboratorio/admin.py
+++ Laboratorio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Laboratorio, DirectorGeneral, Producto
 # Register your models here.
+
+
+@admin.register(Laboratorio)
+class LaboratorioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DirectorGeneral)
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    class Meta:
+        verbose_name = 'Director General'
+        verbose_name_plural = 'Directores Generales'
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
