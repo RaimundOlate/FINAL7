f815e824363fa566a912706c10085dccb710b52c
Raimundo Olate <raimundolatetoro@hotmail.com>
2023-07-18 16:46:25 -0400

    migraciones, pero se me olvidó el nombre:(

--- Laboratorio/models.py
+++ Laboratorio/models.py
@@ -5,6 +5,8 @@ from datetime import date
 
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length=255)
+    ciudad = models.CharField(max_length=255, null=True, blank=True)
+    pais = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
@@ -13,6 +15,7 @@ class Laboratorio(models.Model):
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=255)
     laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
