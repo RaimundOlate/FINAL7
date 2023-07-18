e98897cae7acad6b318987f5e08ccabb0c1be88f
Raimundo Olate <raimundolatetoro@hotmail.com>
2023-07-18 15:52:09 -0400

    se agregan los laboratorios y el def__str__

--- Laboratorio/models.py
+++ Laboratorio/models.py
@@ -5,10 +5,16 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
+
 
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=255)
     laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Producto(models.Model):
@@ -20,3 +26,6 @@ class Producto(models.Model):
     ])
     p_costo = models.DecimalField(max_digits=10, decimal_places=2)
     p_venta = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
