915b5399df13acad36b44289430fd32b116bb0a3
Raimundo Olate <raimundolatetoro@hotmail.com>
2023-07-18 18:15:54 -0400

    se agregan 2 test, el ultimo no lo entendi

--- Laboratorio/tests.py
+++ Laboratorio/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Laboratorio
 
-# Create your tests here.
+
+class LaboratorioModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Ejemplo de test
+        Laboratorio.objects.create(
+            nombre='Laboratorio 1', ciudad='Ciudad 1', pais='País 1')
+
+    def test_datos_laboratorio_coinciden(self):
+        laboratorio = Laboratorio.objects.get(nombre='Laboratorio 1')
+        self.assertEqual(laboratorio.nombre, 'Laboratorio 1')
+        self.assertEqual(laboratorio.ciudad, 'Ciudad 1')
+        self.assertEqual(laboratorio.pais, 'País 1')
+
+
+class LaboratorioURLTest(TestCase):
+    def test_laboratorio_url_responde_con_exito(self):
+        client = Client()
+        response = client.get('/laboratorio/')
+        self.assertEqual(response.status_code, 200)
